13109e98979834f06fb1d83d6ffa295be6474d9e
Gokul C <mm20b020@smail.iitm.ac.in>
2023-04-26 21:39:10 +0530

    Updated 4 digit seven segment display

--- firmware/sevseg_four_digit/main.c
+++ firmware/sevseg_four_digit/main.c
@@ -94,6 +94,7 @@ int sevseg(int num, int digit) // Function to display on segment
 		hex = ~hex;
 	reg_mprj_datal = hex;
 	reg_mprj_datah = 0xFFFFFFFF & (1 << (digit + 1));
+    //reg_mprj_datah = 0xFFFFFFFF;
 }
 
 void main()
@@ -108,7 +109,7 @@ void main()
 	int b = 0;
 	int c = 0;
 	int d = 0;
-
+    int k = 0;
 	// while (1)
 	// {
 	// 	for (int i = 1; i < 5; i++)
@@ -123,22 +124,48 @@ void main()
 
 	while (1)
 	{
-		a = count / 1000;
-		b = (count % 1000) / 100;
-		c = (count % 100) / 10;
-		d = count % 10;
-		for (int k; k < 1000; k++)
+		//a = count / 1000;
+      // a = count >> 10;
+		//b = (count % 1000) / 100;
+		//c = (count % 100) / 10;
+		//d = count % 10;
+        //b = ((count & 0x3E8) >> 8);
+        //c = ((count & 0x64) >> 4);
+        //d = (count & 0xA) >> 1;
+       //a = 1;
+       //b = 1;
+       //c = 1;
+       //d = 1;
+	   for (k = 0; k < 100; k++)
 		{
 			sevseg(a, 4);
-			delay(10000);
+			delay(100);
 			sevseg(b, 3);
-			delay(10000);
-			sevseg(c, 3);
-			delay(10000);
+			delay(100);
+			sevseg(c, 2);
+			delay(100);
 			sevseg(d, 1);
-			delay(10000);
+			delay(100);
 		}
+        c += 1;
+        if(c>9){
+           b+=1;
+           c=0;
+           if(b>9){
+               a+=1;
+               b=0;
+               if(a>9)
+                  a=0;
+            }
+        }
 
 		count++;
+        //if(count == 10){
+          //  count = 0;
+           // a+=1;
+            //if(a > 8)
+             //   a = 0;
+        //}
+
 	}
 }
