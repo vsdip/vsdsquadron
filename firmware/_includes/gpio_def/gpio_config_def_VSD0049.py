4599e723759715a5b84df2cbb8a09d6977b578a6
Robotics <rakshith.ec20@bmsce.ac.in>
2023-07-03 15:05:50 +0530

    uploading def files

--- firmware/_includes/gpio_def/gpio_config_def_VSD0049.py
+++ firmware/_includes/gpio_def/gpio_config_def_VSD0049.py
@@ -1,4 +1,4 @@
-# gpio_config_def.py file for part VSD0044
+# gpio_config_def.py file for part VSD0049
 # io_config -- version 1.2.1
 voltage = 1.60
 analog = False
@@ -10,35 +10,35 @@ H_SPECIAL     = 3
 H_UNKNOWN     = 4  
 
 # voltage: 1.6
-# configuration failed in gpio[16], anything after is invalid
+# configuration failed in gpio[1], anything after is invalid
 gpio_l = [
 ['IO[0]', H_NONE],
-['IO[1]', H_INDEPENDENT],
-['IO[2]', H_DEPENDENT],
-['IO[3]', H_DEPENDENT],
-['IO[4]', H_DEPENDENT],
-['IO[5]', H_INDEPENDENT],
-['IO[6]', H_DEPENDENT],
-['IO[7]', H_DEPENDENT],
-['IO[8]', H_DEPENDENT],
-['IO[9]', H_DEPENDENT],
-['IO[10]', H_DEPENDENT],
-['IO[11]', H_INDEPENDENT],
-['IO[12]', H_DEPENDENT],
-['IO[13]', H_DEPENDENT],
-['IO[14]', H_INDEPENDENT],
-['IO[15]', H_INDEPENDENT],
+['IO[1]', H_UNKNOWN],
+['IO[2]', H_UNKNOWN],
+['IO[3]', H_UNKNOWN],
+['IO[4]', H_UNKNOWN],
+['IO[5]', H_UNKNOWN],
+['IO[6]', H_UNKNOWN],
+['IO[7]', H_UNKNOWN],
+['IO[8]', H_UNKNOWN],
+['IO[9]', H_UNKNOWN],
+['IO[10]', H_UNKNOWN],
+['IO[11]', H_UNKNOWN],
+['IO[12]', H_UNKNOWN],
+['IO[13]', H_UNKNOWN],
+['IO[14]', H_UNKNOWN],
+['IO[15]', H_UNKNOWN],
 ['IO[16]', H_UNKNOWN],
 ['IO[17]', H_UNKNOWN],
 ['IO[18]', H_UNKNOWN],
 ]
 # voltage: 1.6
-# configuration failed in gpio[33], anything before is invalid
+# configuration failed in gpio[35], anything before is invalid
 gpio_h = [
 ['IO[37]', H_NONE],
-['IO[36]', H_DEPENDENT],
-['IO[35]', H_INDEPENDENT],
-['IO[34]', H_DEPENDENT],
+['IO[36]', H_INDEPENDENT],
+['IO[35]', H_UNKNOWN],
+['IO[34]', H_UNKNOWN],
 ['IO[33]', H_UNKNOWN],
 ['IO[32]', H_UNKNOWN],
 ['IO[31]', H_UNKNOWN],
