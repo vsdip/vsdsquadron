015b3199aadb0a5516800612a914a54895e91a21
Visruat <visruattr@gmail.com>
2023-08-15 10:54:55 +0530

    Uploading def files for 12 boards (8 passed 4 failed) spread across Batches 1,2,3. Adding missing def file for VSD0128 and modified VSD0134

--- firmware/_includes/gpio_def/gpio_config_def_VSD0033.py
+++ firmware/_includes/gpio_def/gpio_config_def_VSD0033.py
@@ -1,4 +1,4 @@
-# gpio_config_def.py file for part VSD0134
+# gpio_config_def.py file for part VSD0033
 # io_config -- version 1.2.1
 voltage = 1.60
 analog = False
@@ -10,16 +10,16 @@ H_SPECIAL     = 3
 H_UNKNOWN     = 4  
 
 # voltage: 1.6
-# configuration failed in gpio[8], anything after is invalid
+# configuration failed in gpio[2], anything after is invalid
 gpio_l = [
 ['IO[0]', H_NONE],
-['IO[1]', H_DEPENDENT],
-['IO[2]', H_DEPENDENT],
-['IO[3]', H_DEPENDENT],
-['IO[4]', H_DEPENDENT],
-['IO[5]', H_INDEPENDENT],
-['IO[6]', H_INDEPENDENT],
-['IO[7]', H_INDEPENDENT],
+['IO[1]', H_INDEPENDENT],
+['IO[2]', H_UNKNOWN],
+['IO[3]', H_UNKNOWN],
+['IO[4]', H_UNKNOWN],
+['IO[5]', H_UNKNOWN],
+['IO[6]', H_UNKNOWN],
+['IO[7]', H_UNKNOWN],
 ['IO[8]', H_UNKNOWN],
 ['IO[9]', H_UNKNOWN],
 ['IO[10]', H_UNKNOWN],
@@ -33,25 +33,25 @@ gpio_l = [
 ['IO[18]', H_UNKNOWN],
 ]
 # voltage: 1.6
-# configuration failed in gpio[25], anything before is invalid
+# configuration failed in gpio[19], anything before is invalid
 gpio_h = [
 ['IO[37]', H_NONE],
-['IO[36]', H_DEPENDENT],
+['IO[36]', H_INDEPENDENT],
 ['IO[35]', H_DEPENDENT],
-['IO[34]', H_DEPENDENT],
+['IO[34]', H_INDEPENDENT],
 ['IO[33]', H_INDEPENDENT],
 ['IO[32]', H_INDEPENDENT],
 ['IO[31]', H_INDEPENDENT],
-['IO[30]', H_INDEPENDENT],
-['IO[29]', H_INDEPENDENT],
+['IO[30]', H_DEPENDENT],
+['IO[29]', H_DEPENDENT],
 ['IO[28]', H_DEPENDENT],
 ['IO[27]', H_INDEPENDENT],
-['IO[26]', H_INDEPENDENT],
-['IO[25]', H_UNKNOWN],
-['IO[24]', H_UNKNOWN],
-['IO[23]', H_UNKNOWN],
-['IO[22]', H_UNKNOWN],
-['IO[21]', H_UNKNOWN],
-['IO[20]', H_UNKNOWN],
+['IO[26]', H_DEPENDENT],
+['IO[25]', H_DEPENDENT],
+['IO[24]', H_INDEPENDENT],
+['IO[23]', H_INDEPENDENT],
+['IO[22]', H_INDEPENDENT],
+['IO[21]', H_INDEPENDENT],
+['IO[20]', H_INDEPENDENT],
 ['IO[19]', H_UNKNOWN],
 ]
